c2bc397225fee00f959f2715ffe88bb5d109be49
Fabricio-Luna47 <fabricio.luna@.estudante.ifms.edu.br>
2022-12-02 23:05:48 -0400

    codigo atualizado

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,36 @@
 import '../css/style.css'
-/* Selecionar os Elementos no DOM */
 
-const iputPesquisa = 
-document.querySelector("#inputPesquisa")
-const inputPesquisa = 
-document.querySelector("#btnLocalizar")
-const pokedexDisplay = 
-ocment.querySelector("display")
+/* SELECIONAR OS ELEMENTOS DO DOM */
 
-/*Adicionando Eventos */
+const inputPesquisa = document.querySelector("#inputPesquisa")
+const btnLocalizar = document.querySelector("#btnLocalizar")
+const pokedexDisplay = document.querySelector("#display")
 
-btnLocalizar.AddEventListener(click, function(){
-    /* buscar dados do pokemon na api */
-    const dadosDoPokemon = await LocalizarPokemon(inputPesquisa.value)
-    /* criar cartao do pokemon */
-    criarCartao()
+/* Adicionando eventos */
+
+btnLocalizar.addEventListener('click',async function(){ 
+    /* buscar os dados do pokemon na api */
+   const dadosDoPokemon = await localizarPokemon(inputPesquisa.value)
+  /* criar o cartao do pokemon */
+   criarCartao(dadosDoPokemon)
+
 })
 
-async function btnLocalizarPokemon(termoBusca){
-    const url = 
-    https://pokeapi.co/api/v2/pokemon/${termoBusca}
-    const response = await fetch(url) 
-    const pokemon = await response.json() 
+async function localizarPokemon(termoBusca){
+    const url = `https://pokeapi.co/api/v2/pokemon/${termoBusca}`
+    const response = await fetch(url)
+    const pokemon = await response.json()
     console.log(pokemon)
-    return pokemon 
-
+    return pokemon
 }
 
-function criarCatao(pokemon){
-    const cartaoPokemon  = document.createElement('div')
+function criarCartao(pokemon){
+    const cartaoPokemon = document.createElement('div')
     cartaoPokemon.className='cartaoPokemon'
     cartaoPokemon.innerHTML=`
-    <img  class="pokemon.Sprites" src="$Pokemon.sprites.front_default}"/>
-    <h2>${pokemon.classname}<h2
+    <img class= "pokemonSprite" src="${pokemon.sprites.front_default}"/>
+    <h2>${pokemon.name}</h2>
 
-
+    `
+    pokedexDisplay.appendChild(cartaoPokemon)
 }
